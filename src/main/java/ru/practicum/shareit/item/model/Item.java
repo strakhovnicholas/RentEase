46d9adc888610dcd31d7528772afc0ef0f6c2cd7
Nicholas <strahovnikolaj27@gmail.com>
2025-11-21 17:04:06 +0300

    Доработана сущность Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.shareit.request.model.Request;
+import ru.practicum.shareit.user.entity.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean available;
+    private User owner;
+    private Request request;
 }
