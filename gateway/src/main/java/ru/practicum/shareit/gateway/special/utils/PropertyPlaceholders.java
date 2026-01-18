aa7666254724f8c99e713bd652277afa86ba6aa2
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2026-01-19 00:26:41 +0300

    fix: checkstyle error - move closing brace to new line in PropertyPlaceholders

--- gateway/src/main/java/ru/practicum/shareit/gateway/special/utils/PropertyPlaceholders.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/special/utils/PropertyPlaceholders.java
@@ -3,5 +3,6 @@ package ru.practicum.shareit.gateway.special.utils;
 public final class PropertyPlaceholders {
     public static final String SERVER_URL = "${shareit-server.url}";
 
-    private PropertyPlaceholders() {}
+    private PropertyPlaceholders() {
+    }
 }
